f01e1089b209c0e55c4a12326505167713ebf035
nibeon <nibeon@gmail.com>
2022-08-07 22:04:05 +0300

    remake site
    Signed-off-by: nibeon <nibeon@gmail.com>

---
+++ contact_form_spectrecss.md
@@ -0,0 +1,50 @@
+ # contact form on Spectre CSS
+ 
+ 
+                <form class="form-horizontal">
+                      <div class="form-group">
+                        <div class="col-3 col-sm-12">
+                          <label class="form-label" for="input-name">Name</label>
+                        </div>
+                        <div class="col-9 col-sm-12">
+                          <input class="form-input" type="text" id="input-name" placeholder="Your Name" disabled>
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <div class="col-3 col-sm-12">
+                          <label class="form-label" for="input-email">Email</label>
+                        </div>
+                        <div class="col-9 col-sm-12">
+                          <input class="form-input" type="text" id="input-email" placeholder="your_email@example.com" disabled>
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <div class="col-3 col-sm-12">
+                          <label class="form-label" for="input-subject">Subject</label>
+                        </div>
+                        <div class="col-9 col-sm-12">
+                          <input class="form-input" type="text" id="input-subject" placeholder="About site" disabled>
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <div class="col-3 col-sm-12">
+                          <label class="form-label" for="input-msg">Message</label>
+                        </div>
+                        <div class="col-9 col-sm-12"> 
+                          <textarea class="form-input" name="" id="input-msg" disabled>Your message</textarea>
+                          
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <div class="col-3 col-sm-12">
+
+                        </div>
+                        <div class="col-9 col-sm-12">
+                          <a class="btn btn-primary" id="send-mail" href="mailto:nibeon@gmail.com&subject=About&site">Send mail</a>
+                        </div>
+                      </div>
+                    </form>
--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    $('.tabs').tabs();
-    
-    // плавный скроллинг к внутренним ссылкам
-    $("#nav-mobile").on("click","a", function (event) {
+    // плавный скроллинг по внутренним ссылкам
+    $("#nav").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
 
@@ -15,5 +13,92 @@ $(document).ready(function() {
 		//анимируем переход на расстояние - top за 1500 мс
 		$('body,html').animate({scrollTop: top}, 1500);
 	});
+    
+    // появление книпки "наверх" при прокрутке
+    $(window).scroll(function(){
+		if($(window).scrollTop()>120){
+			$('#to-top').fadeIn(700);
+		}else{
+			$('#to-top').fadeOut(700);
+		}
+	});
+    
+    
+    // сколлинг наверх при нажатии на кнопку "наверх"
+    $(".to-top").click(function(event){
+        //забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+    
+    $(".img").mouseover(function(event) {
+        $(this).children(".hover").css("display", "block");
+    });
+    
+    $(".img").mouseout(function(event) {
+        $(this).children(".hover").css("display", "none");
+    });
+    
+    
+    // открытие и закрытие модальных окон
+    
+    // 1
+    $("#for-modal-1").click(function(event) {
+        $("#modal-1").addClass("active");
+    });    
+    $(".close-modal-1").click(function(event) {
+        $("#modal-1").removeClass("active");
+    });    
+    
+    // 2
+    $("#for-modal-2").click(function(event) {
+        $("#modal-2").addClass("active");
+    });    
+    $(".close-modal-2").click(function(event) {
+        $("#modal-2").removeClass("active");
+    }); 
+    
+    // 3
+    $("#for-modal-3").click(function(event) {
+        $("#modal-3").addClass("active");
+    });    
+    $(".close-modal-3").click(function(event) {
+        $("#modal-3").removeClass("active");
+    }); 
+    
+    // 4
+    $("#for-modal-4").click(function(event) {
+        $("#modal-4").addClass("active");
+    });    
+    $(".close-modal-4").click(function(event) {
+        $("#modal-4").removeClass("active");
+    }); 
+    
+    // 5
+    $("#for-modal-5").click(function(event) {
+        $("#modal-5").addClass("active");
+    });    
+    $(".close-modal-5").click(function(event) {
+        $("#modal-5").removeClass("active");
+    }); 
+    
+     // 5
+    $("#for-modal-6").click(function(event) {
+        $("#modal-6").addClass("active");
+    });    
+    $(".close-modal-6").click(function(event) {
+        $("#modal-6").removeClass("active");
+    });    
+    
+    // 
+    $(".modal-footer button").hover(function(event){
+       $(this).addClass("btn-primary");
+    });
     
 });
